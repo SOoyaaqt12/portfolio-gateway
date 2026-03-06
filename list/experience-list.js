f1292619dbf7bcd5a455ed897b3c109b85faae58
PC-2022-030 <daffarafdhan@gmail.com>
2026-03-06 11:23:15 +0700

    update timeline

--- list/experience-list.js
+++ list/experience-list.js
@@ -4,7 +4,7 @@ const experienceData = [
         id: 1,
         title: "Web Developer Intern",
         company: "Larosa Pot",
-        date: "Jan 2024 - Sekarang",
+        date: "November 2025 - Februari 2026",
         description: "Membuat website korporat untuk larosa pot, dan juga membuat sistem kasir Larosa.",
     },
 ];
